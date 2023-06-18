08674b921a729929541bf976fb7f336efa88390a
Pranay Kothari <pranay.kothari109@gmail.com>
2023-06-18 23:43:52 +0530

    add reset button, timer functionality is working

--- src/app/components/displayBox.tsx
+++ src/app/components/displayBox.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-function DisplayBox({ title, value, pause }) {
+function DisplayBox({ title, value, pause, changeTime }) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const field = e.target.name === 'MM' ? 'm' : 's'
+    const value = e.target.value
+    changeTime(field, value)
+  }
+
   return (
     <div className="flex flex-col">
       <label className="text-gray-700 text-lg font-semibold mb-2">
@@ -8,9 +14,11 @@ function DisplayBox({ title, value, pause }) {
       </label>
       {pause ? (
         <input
-          type="text"
+          name={title}
+          type="number"
           className="text-right w-20 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           value={value}
+          onChange={handleChange}
         />
       ) : (
         <div className="w-20 px-3 py-2">{value}</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,75 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import DisplayBox from './components/displayBox'
 
 export default function Home() {
-  // const [{ m, s }, setTime] = useState({
-  //   m: 1,
-  //   s: 1
-  // });
+  const [{ m, s }, setTime] = useState({
+    m: 1,
+    s: 1,
+  })
   const [pause, setPause] = useState(false)
 
-  // useEffect(() => {
-  //   if (m === 0 && s === 0) {
-  //     return;
-  //   }
-  //   if (s === 0) {
-  //     return setTime(() => ({ m: m - 1, s: 59 }));
-  //   }
-  //   const timer = setInterval(() => {
-  //     if (!pause) {
-  //       setTime(() => ({ m: m, s: s - 1 }));
-  //     }
-  //     clearInterval(timer);
-  //   }, 1000);
-  // }, [m, s, pause]);
+  useEffect(() => {
+    if (m === 0 && s === 0) {
+      return
+    }
+    if (s === 0) {
+      return setTime(() => ({ m: m - 1, s: 59 }))
+    }
+    const timer = setInterval(() => {
+      if (!pause) {
+        setTime(() => ({ m: m, s: s - 1 }))
+      }
+      clearInterval(timer)
+    }, 1000)
+  }, [m, s, pause])
 
-  // const handleStart = () => {
+  const handlePause = () => {
+    setPause(() => !pause)
+  }
 
-  // }
+  const changeTime = (param: string, value: number) => {
+    setTime((prev) => ({ ...prev, [param]: value }))
+  }
 
-  // const handlePause = () => {
-  //   setPause(() => !pause);
-  // };
-
-  // const handleReset = () => {
-  //   setPause(() => true); // stopping timer
-  //   setTime(() => ({ m: 1, s: 1 })); // setting timer
-  //   setPause(() => false); // starting timer again
-  // };
+  const handleReset = () => {
+    setPause(() => true) // stopping timer
+    setTime(() => ({ m: 1, s: 1 })) // setting timer
+  }
 
   return (
     <main className="min-h-screen p-24">
       <div className="text-5xl text-center">Timer</div>
       <div className="flex flex-row gap-3 justify-center text-center mt-[7%] mb-5">
-        <DisplayBox title="MM" value={9} pause={pause} />
-        <DisplayBox title="SS" value={9} pause={pause} />
+        <DisplayBox
+          title="MM"
+          value={m}
+          pause={pause}
+          changeTime={changeTime}
+        />
+        <DisplayBox
+          title="SS"
+          value={s}
+          pause={pause}
+          changeTime={changeTime}
+        />
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center gap-2">
         <button
           className="rounded-full bg-indigo-300 h-16 w-16"
-          onClick={() => setPause(!pause)}
+          onClick={handlePause}
         >
-          Start
+          {pause ? 'Start' : 'Stop'}
         </button>
+        {pause && (
+          <button
+            className="rounded-full bg-indigo-300 h-16 w-16"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        )}
       </div>
     </main>
   )
